363c44d1874bc4b6e2cf1c8943fc5c1b85b186d1
Moises Miguel <moimigueldev@gmail.com>
2025-02-20 02:46:10 -0600

    added deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/marias_spanish_site/', // Add this line
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,3 +16,4 @@ export default defineConfig({
     },
   },
 })
+
