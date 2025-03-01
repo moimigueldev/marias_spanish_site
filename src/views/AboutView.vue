016a162afc34b86f6e23621c2f5ff0dcc58aede8
Moises Miguel <moimigueldev@gmail.com>
2025-03-01 13:31:43 -0600

    update about me page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,50 +1,63 @@
 <script setup></script>
 
 <template>
-  <div class="flex h-200 flex-col gap-5 lg:flex-row">
+  <div class="flex">
     <div
-      class="left relative w-full rounded-lg bg-orange-300 text-center shadow-lg lg:w-1/2 lg:p-20"
+      class="relative mx-auto w-full rounded-lg bg-orange-300 p-10 text-center shadow-lg lg:w-2/3 lg:p-10"
     >
-      <img
-        src="@/assets/mexican_banner_1.webp"
-        alt="mexican banner"
-        class="absolute top-0 right-0 left-0 hidden h-[400px] w-full lg:block"
-        loading="eager"
-        fetchpriority="high"
-        width="1200"
-        height="400"
-      />
+      <!-- <img src="@/assets/mexican_banner_1.webp" alt="mexican banner" -->
+      <!--   class="absolute top-0 right-0 left-0  h-[200px] lg:h-[400px] " loading="eager" fetchpriority="high" /> -->
       <img
         src="@/assets/profile_image.webp"
-        class="mx-auto my-10 h-60 w-60 rounded-full object-cover shadow-lg sm:block lg:hidden lg:h-150 lg:w-150"
+        class="mx-auto size-60 rounded-full object-cover shadow-xl lg:size-90"
         alt="Profile Image"
         loading="eager"
         fetchpriority="high"
-        width="240"
-        height="240"
       />
-      <p class="text-2xl font-bold lg:mt-30 lg:text-5xl">Maria Estrada Miguel</p>
-      <p class="text-lg font-extralight font-semibold lg:mt-5 lg:text-xl">About Me</p>
+      <p class="my-5 text-2xl font-bold lg:text-5xl">Maria Estrada Miguel</p>
 
-      <p class="p-5 text-sm font-light lg:text-lg">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi illum labore soluta
-        aperiam vero inventore laborum, culpa quisquam ratione placeat, ipsa nam earum ea
-        repellendus aspernatur cupiditate, pariatur dolor maiores? Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptatibus nemo illo illum pariatur fugit! Ea suscipit
-        accusamus praesentium facere doloremque. Recusandae quam delectus cumque ipsa, expedita
-        nesciunt earum libero quaerat?
+      <p class="text-left text-sm font-light lg:text-lg">
+        Hello! I’m Maria Estrada Miguel. Growing up, I spoke Spanish at home, which gave me a deep
+        appreciation for the beauty and richness of the language. With most of my family living in
+        Mexico City, I feel incredibly blessed to travel there often and stay connected to my roots.
+        Although I studied finance at the University of Minnesota, my passion for teaching Spanish
+        has always been a big part of who I am.
+
+        <br />
+        <br />
+        Spanish has been a part of my life for as long as I can remember. Since the moment I could
+        speak, I’ve been translating for the people around me—family, friends, and even strangers
+        who needed help. Can you imagine a 6-year-old translating at a doctor’s office or trying to
+        make sense of legal documents for their parents? That was my reality, and honestly, it
+        shaped who I am today. Translating became second nature to me, whether it was helping my
+        family, assisting Spanish-speaking individuals at work, or even planning events like Cinco
+        de Mayo celebrations for the neighborhood. It’s not just about knowing two languages—it’s
+        about bridging gaps, making people feel heard, and ensuring they get the help they need.
+
+        <br />
+        <br />
+
+        Growing up bilingual has made a huge difference in my life, especially when it comes to
+        connecting with people. Back in high school, I tutored elementary kids, which helped me
+        build patience and communication skills—both of which have been essential in my work and
+        volunteer experiences. I’ve worked with a lot of Spanish-speaking individuals, whether
+        through translating documents, having in-person conversations, or volunteering at places
+        like Regions Hospital and the Animal Humane Society. Every time I’m able to help someone
+        express themselves, I’m reminded of how valuable it is to know two languages. I feel
+        grateful for the opportunities it has given me—not just to open doors for myself, but to
+        open them for others too.
       </p>
     </div>
-    <div class="rigt flex w-full items-center justify-center rounded-lg lg:w-1/2">
-      <img
-        src="@/assets/profile_image.webp"
-        class="hidden h-60 w-60 rounded-full object-cover shadow-lg lg:block lg:h-150 lg:w-150"
-        alt="Profile Image"
-        loading="eager"
-        fetchpriority="high"
-        width="240"
-        height="240"
-      />
-    </div>
+    <!-- <div class="rigt flex w-full items-center justify-center rounded-lg lg:w-1/2"> -->
+    <!--   <img -->
+    <!--     src="@/assets/profile_image.webp" -->
+    <!--     class="hidden h-60 w-60 rounded-full object-cover shadow-lg lg:block lg:h-150 lg:w-150" -->
+    <!--     alt="Profile Image" -->
+    <!--     loading="eager" -->
+    <!--     fetchpriority="high" -->
+    <!--     width="240" -->
+    <!--     height="240" -->
+    <!--   /> -->
+    <!-- </div> -->
   </div>
 </template>
